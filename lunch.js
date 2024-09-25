64315bd197612fe3b063cbdad64a8840c89faf67
Oliviaa7 <olivialanderson1994@gmail.com>
2024-09-25 14:19:48 -0700

    Completed assignment whats-for-lunch

--- lunch.js
+++ lunch.js
@@ -14,26 +14,35 @@
  */
 
 const whatToDoForLunch = function(hungry, availableTime) {
-  console.log("I don't know what to do!");
+
+  if (hungry === false) {
+
+    return "Wait until you're hungry to take a break."
+  } else if (hungry === true && availableTime < 20) {
+
+    return "Pick up a snack or grab something you have ready at home."
+  } else if (hungry === true && availableTime >= 20 && availableTime <= 30) {
+
+    return "You deserve a break and should take time to cook a tasty meal."
+  } else if (hungry === true && availableTime > 30) {
+
+    return "This is an intense program, you should probably reconsider..."
+  }
+
 }
 
 
-/*
- * This is some test runner code that's simply calling our whatToDoForLunch function
- * defined above to verify we're making the right decisions. Do not modify it!
- */
-
 console.log("I'm hungry and I have 20 minutes for lunch.");
-whatToDoForLunch(true, 20);
+console.log(whatToDoForLunch(true, 20));
 console.log("---");
 
 console.log("I'm hungry and I have 50 minutes for lunch.");
-whatToDoForLunch(true, 50);
+console.log(whatToDoForLunch(true, 50));
 console.log("---");
 
 console.log("I'm not hungry and I have 30 minutes for lunch.");
-whatToDoForLunch(false, 30);
+console.log(whatToDoForLunch(false, 30));
 console.log("---");
 
 console.log("I'm hungry and I have 15 minutes for lunch.");
-whatToDoForLunch(true, 15);
+console.log(whatToDoForLunch(true, 15));
